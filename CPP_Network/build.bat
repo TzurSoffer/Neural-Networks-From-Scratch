5b3df07419cae85fa2bb793c94130b9e1119dc07
Tzur Soffer <tzur.soffer@gmail.com>
2026-06-11 18:28:11 -0700

    build.bat

--- CPP_Network/build.bat
+++ CPP_Network/build.bat
@@ -3,5 +3,5 @@ py -3.14 setup_mathlib.py build
 
 py -3.14 setup.py install
 py -3.14 setup_mathlib.py install
-@REM sudo py -3.13 setup.py install
-@REM sudo py -3.13 setup_mathlib.py install
+sudo py -3.13 setup.py install
+sudo py -3.13 setup_mathlib.py install
